1d57b8259e5187e39874b81ff91abad89eae3548
Bobby Xia <xiamubobby@gmail.com>
2011-06-07 03:02:37 +0800

    the orig

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inoroy::Application.config.secret_token = 'feeeea1c46448a1674119d482d0d793f4264c215c07ff7b65fc0bd4d1f852bf318dd01da4cb47b707b36daedcde43bf7b29a498d88dd7a64bb7f95b629be1e7d'
